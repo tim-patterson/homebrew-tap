ba422259c700a294703882d5e1d1fee7fbf328bf
Tim Patterson <tim.wanaka@gmail.com>
2019-10-28 12:03:45 +1300

    updated sha

--- Formula/jsonsql.rb
+++ Formula/jsonsql.rb
@@ -2,7 +2,7 @@ class Jsonsql < Formula
   desc "A simple cli to run sql against json"
   homepage "https://github.com/tim-patterson/jsonsql"
   url "https://github.com/tim-patterson/jsonsql/releases/download/v1.0.6/jsonsql.tar.gz"
-  sha256 "ad415b920707164a307226268fd3b8ee014df22a72afc370a2159182ecaa3026"
+  sha256 "2949d24f32ef0621a107c28d757a8fb42802dd1824e9d0306d5ad8b64c0af22f"
 
   def install
     prefix.install Dir["."]
