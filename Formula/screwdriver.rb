c8f5f750587e620b0bf5eb758d0e1e6521332021
Tim Patterson <tim.wanaka@gmail.com>
2019-11-03 23:42:58 +1300

    bump screwdriver

--- Formula/screwdriver.rb
+++ Formula/screwdriver.rb
@@ -1,8 +1,8 @@
 class Screwdriver < Formula
   desc "A collection of small scripts to help with data/aws type tasks"
   homepage "https://github.com/tim-patterson/screwdriver"
-  url "https://github.com/tim-patterson/screwdriver/releases/download/v0.1.1/screwdriver.tar.gz"
-  sha256 "bc165ac8ac67f3d97b3a3d8fee27fe8f5049cd2a9752395baabbc3bbb7979c8d"
+  url "https://github.com/tim-patterson/screwdriver/releases/download/v0.1.3/screwdriver.tar.gz"
+  sha256 "a4979bb46c8ccd531e2295a6fceb70b76bacd2adc5857040ffe1fc94620e2a1b"
 
   def install
     prefix.install Dir["screwdriver"]
