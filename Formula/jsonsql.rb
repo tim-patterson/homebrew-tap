c24ccf117843dd025850ee5dad5dbdca406925a7
Tim Patterson <tim.wanaka@gmail.com>
2020-02-07 10:49:30 +1300

    Bump deps

--- Formula/jsonsql.rb
+++ Formula/jsonsql.rb
@@ -1,8 +1,8 @@
 class Jsonsql < Formula
   desc "A simple cli to run sql against json"
   homepage "https://github.com/tim-patterson/jsonsql"
-  url "https://github.com/tim-patterson/jsonsql/releases/download/v1.0.9/jsonsql.osx.tar.gz"
-  sha256 "5b338c3eae130416c515c858db280ce50c91eeefb221e5abca5af497cea830b6"
+  url "https://github.com/tim-patterson/jsonsql/releases/download/v1.0.10/jsonsql.osx.tar.gz"
+  sha256 "f897dd09fd67cc43382a969133c06dfb27695948e7262d9b4b788308ec91d22d"
 
   def install
     bin.install "jsonsql"
